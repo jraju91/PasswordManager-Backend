263e4bce1905457860f8daf2fff91c3d349c4bfe
jraju01 <jamesraju91@gmail.com>
2022-07-21 22:36:25 -0400

    connection_url

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const mongoURI =
-  process.env.NODE_ENV === 'production'
-    ? process.env.DB_URL
-    : 'mongodb+srv://test:test@cluster0.0evvb.mongodb.net/passwordapp?retryWrites=true&w=majority';
+  process.env.NODE_ENV === "production"
+    ? process.env.CONNECTION_URL
+    : "mongodb+srv://test:test@cluster0.0evvb.mongodb.net/passwordapp?retryWrites=true&w=majority";
 mongoose
   .connect(mongoURI)
   .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
-  .catch((error) => console.log('Connection failed!', error));
+  .catch((error) => console.log("Connection failed!", error));
 
 export default mongoose;
